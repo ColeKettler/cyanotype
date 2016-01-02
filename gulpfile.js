cff8975fb4f489e4873a383a80f01bdf67165d88
Cole Kettler <me@colekettler.com>
2016-01-02 18:50:48 -0500

    Add gulp build tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,49 @@
 var cp = require('child_process');
 var path = require('path');
 
+var del = require('del');
 var gulp = require('gulp');
 var cache = require('gulp-cached');
 var jshint = require('gulp-jshint');
 
 // FILES
+var cssFiles = ['assets/css/**/*.css'];
+var fontFiles = ['assets/fonts/**/*.{eot,svg,ttf,woff,woff2}'];
+var hbsFiles = ['*.hbs', 'partials/*.hbs'];
+var imageFiles = ['assets/images/**/*.{gif,jpg,jpeg,png}'];
 var jsFiles = ['gulpfile.js', 'assets/js/**/*.js'];
+var jsonFiles = ['package.json'];
+var allFiles = Array.prototype.concat.apply([], [
+  cssFiles,
+  fontFiles,
+  hbsFiles,
+  imageFiles,
+  jsFiles,
+  jsonFiles,
+]);
+
+// GHOST
+var ghostDir = process.env.GHOST_DIR || path.resolve('../ghost');
+var themeDir = path.join(ghostDir, 'content', 'themes', 'cyanotype');
 
 // TASKS
-gulp.task('default', ['ghost', 'lint', 'watch']);
+gulp.task('default', ['build', 'ghost', 'lint', 'watch']);
+
+gulp.task('build', ['clean', 'copy']);
+
+gulp.task('clean', function() {
+  return del(themeDir, { force: true });
+});
+
+gulp.task('copy', ['clean'], function () {
+  return gulp.src(allFiles, { base: '.' })
+    .pipe(gulp.dest(themeDir));
+});
+
+// Run Ghost in development mode.
+gulp.task('ghost', ['build'], function () {
+  cp.spawn('npm', ['start'], { cwd: ghostDir, stdio: 'inherit' });
+});
 
 gulp.task('lint', function () {
   return gulp.src(jsFiles)
@@ -20,12 +54,7 @@ gulp.task('lint', function () {
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
-// Run Ghost in development mode.
-gulp.task('ghost', function () {
-  var ghostDir = process.env.GHOST_DIR || path.resolve('../ghost');
-  cp.spawn('npm', ['start'], { cwd: ghostDir, stdio: 'inherit' });
-});
-
 gulp.task('watch', function () {
+  gulp.watch(allFiles, ['copy']);
   gulp.watch(jsFiles, ['lint']);
 });
